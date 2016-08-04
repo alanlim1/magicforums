0e08356629c7debf062f1d2331f77619b9d8fe77
Alan <alanoidster@gmail.com>
2016-08-04 14:35:04 +0800

    Add posts files, edit topics files

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
 
 	def index
-		@topics = Topic.all
+		@topics = Topic.all.order(created_at: :desc)
 	end
 
 	def show
@@ -13,14 +13,44 @@ class TopicsController < ApplicationController
 	end
 
 	def create
-	end
+		@topic = Topic.new(topic_params)
+
+    	if @topic.save
+      		redirect_to topics_path
+    	else
+      		render new_topic_path
+      	end	
+    end
 
 	def edit
+		@topic = Topic.find_by(id: params[:id])
 	end
 
 	def update
+		@topic = Topic.find_by(id: params[:id])
+
+	    if @topic.update(topic_params)
+	    	redirect_to topic_path(@topic)
+    	else
+      		redirect_to edit_topic_path(@topic)
+    	end
 	end
 
 	def destroy
+		@topic = Topic.find_by(id: params[:id])
+    	
+    	if @topic.destroy
+      		redirect_to topics_path
+    	else
+      		redirect_to topic_path(@topic)
+    	end
 	end
-end
+
+
+	private
+
+		def topic_params
+		    params.require(:topic).permit(:title, :description)
+		end
+
+end	
