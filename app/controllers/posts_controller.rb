8fe5474e97e45fa475972c7c3d822f127caf56c1
Alan <alanoidster@gmail.com>
2016-08-05 13:10:46 +0800

    Update controllers, posts and topics files, perfect functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     def index
         @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
         @posts = @topic.posts.order("created_at DESC")
+        @post = Post.new
     end
 
     def new
@@ -33,7 +34,7 @@ class PostsController < ApplicationController
         if @post.update(post_params)      
             redirect_to topic_posts_path(@topic)
         else
-            redirect_to edit_topic_post_path(@topic, @post)
+            redirect_to edit_topic_post_path(@topic)
         end
     end
 
