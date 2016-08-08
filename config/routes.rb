4b25382bdf6a2836c66eacda064fe27f15056ec6
Alan <alanoidster@gmail.com>
2016-08-08 14:30:55 +0800

    Edit posts and topics index files, edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :topics, except: [:show] do
     	resources :posts, except: [:show]
     end
+
+    resources :posts, except: [:show] do
+    	resources :comments
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
